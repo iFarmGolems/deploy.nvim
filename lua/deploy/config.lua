4320870e0144e78b5ef513361b08c2837119b12f
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2024-05-30 09:17:33 +0200

    Clean-up defaults

--- lua/deploy/config.lua
+++ lua/deploy/config.lua
@@ -2,16 +2,22 @@ local M = {}
 
 --@class DeployConfig
 M.defaults = {
+  -- The tool to use for deployment. Default is "rsync".
   tool = "rsync",
+
+  -- Whether to respect .gitignore files when deploying. Default is true.
   honor_gitignore = true,
+
+  -- A table of hosts to which we can deploy. Each host is a table with a 'host' field (a string)
+  -- and a 'label' field (also a string).
+  --@type table<number, {host: string, label: string}>
+  hosts = {},
+
+  -- A table of mappings from local filesystem paths to remote paths.
+  -- Each mapping is a table with a 'fs' field (a string representing a local filesystem path)
+  -- and a 'remote' field (a string representing a remote path).
   --@type table<number, {fs: string, remote: string}>
-  mapping = {
-    {
-      fs = "/home/patrik/develop/repos/mis/sw/ims/ims4/Web/src/main/webapp",
-      remote = "/opt/ims/tomcat/webapps/ims",
-    },
-  },
-  filter = {},
+  mapping = {},
 }
 
 M.options = {}
