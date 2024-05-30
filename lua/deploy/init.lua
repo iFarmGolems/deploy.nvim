0c8d091127992a1e063b6862879a897f17a4f619
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2024-05-31 00:44:26 +0200

    Added auto=false flag

--- lua/deploy/init.lua
+++ lua/deploy/init.lua
@@ -7,7 +7,7 @@ M.setup = config.setup
 M.deploy_current_file = lib.deploy_current_file
 
 vim.api.nvim_create_user_command("DeployCurrentFile", function()
-  M.deploy_current_file()
+  M.deploy_current_file(false)
 end, {})
 
 return M
