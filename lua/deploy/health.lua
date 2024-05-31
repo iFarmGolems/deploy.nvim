7bb5897322e0360157ca342c99042cd6b62aab11
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2024-06-01 00:56:37 +0200

    Better check

--- lua/deploy/health.lua
+++ lua/deploy/health.lua
@@ -3,25 +3,26 @@ local M = {}
 local rsync_exists = vim.fn.executable("rsync")
 local sftp_exists = vim.fn.executable("sftp")
 
-local function check_rsync()
+local function check_tools()
   if not rsync_exists then
     vim.health.warning("rsync executable not found")
   else
     vim.health.ok("rsync executable found")
   end
-end
 
-local function check_sftp()
   if not sftp_exists then
     vim.health.warning("sftp executable not found")
   else
     vim.health.ok("sftp executable found")
   end
+
+  if not rsync_exists or not sftp_exists then
+    vim.health.error("No available deploy tools found!")
+  end
 end
 
 M.check = function()
-  check_rsync()
-  check_sftp()
+  check_tools()
 end
 
 return M
