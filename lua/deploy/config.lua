d20963638a2b65273e24a95aa724e0db7611a50c
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2024-06-01 01:22:40 +0200

    Added typing

--- lua/deploy/config.lua
+++ lua/deploy/config.lua
@@ -1,28 +1,16 @@
 local M = {}
 
---@class DeployConfig
+---@type DeployConfig
 M.defaults = {
-  -- The tool to use for deployment. Default is "rsync".
   tool = "rsync",
-
-  -- Whether to respect .gitignore files when deploying. Default is true.
   honor_gitignore = true,
-
-  -- A table of hosts to which we can deploy. Each host is a table with a 'host' field (a string)
-  -- and a 'label' field (also a string).
-  --@type table<number, {host: string, label: string}>
   hosts = {},
-
-  -- A table of mappings from local filesystem paths to remote paths.
-  -- Each mapping is a table with a 'fs' field (a string representing a local filesystem path)
-  -- and a 'remote' field (a string representing a remote path).
-  --@type table<number, {fs: string, remote: string}>
   mapping = {},
 }
 
-M.options = {}
+M.options = M.defaults
 
---@param opts DeployConfig
+---@param opts DeployConfig
 M.setup = function(opts)
   vim.g.DEPLOY_LAST_HOST = vim.g.DEPLOY_LAST_HOST or "0.0.0.0"
   vim.g.DEPLOY_ON_SAVE = vim.g.DEPLOY_ON_SAVE or false
