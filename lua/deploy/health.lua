5a12ba800ce7c364e68b558652a52fa40b34800e
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2025-11-10 23:58:41 +0100

    Update health checks

--- lua/deploy/health.lua
+++ lua/deploy/health.lua
@@ -1,28 +1,44 @@
 local M = {}
 
 local rsync_exists = vim.fn.executable("rsync")
-local sftp_exists = vim.fn.executable("sftp")
 
-local function check_tools()
+local function check_cli_tools()
   if not rsync_exists then
-    vim.health.warning("rsync executable not found")
+    vim.health.error("rsync executable not found - deploy will not work")
   else
     vim.health.ok("rsync executable found")
   end
+end
 
-  if not sftp_exists then
-    vim.health.warning("sftp executable not found")
+local function check_config()
+  local deploy_config = require("deploy.config").options
+
+  if #deploy_config.hosts == 0 then
+    vim.health.warn("No deploy hosts configured")
   else
-    vim.health.ok("sftp executable found")
+    for _, host in ipairs(deploy_config.hosts) do
+      if not host.address or not host.label then
+        vim.health.error("Deploy host is missing address or label")
+        return
+      end
+    end
   end
 
-  if not rsync_exists or not sftp_exists then
-    vim.health.error("No available deploy tools found!")
+  if #deploy_config.mapping == 0 then
+    vim.health.error("No deploy mappings configured")
+  else
+    for _, map in ipairs(deploy_config.mapping) do
+      if not map.fs or not map.remote then
+        vim.health.error("Deploy mapping is missing fs or remote path")
+        return
+      end
+    end
   end
 end
 
 M.check = function()
-  check_tools()
+  check_cli_tools()
+  check_config()
 end
 
 return M
