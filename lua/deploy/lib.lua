8f4e3cd9e69df6c484cd23c0cef601ca97da2e63
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2025-11-10 20:16:49 +0100

    Utility methods for getting/setting last host (typed)

--- lua/deploy/lib.lua
+++ lua/deploy/lib.lua
@@ -3,6 +3,16 @@ local nio = require("nio")
 
 local M = {}
 
+---@return DeployHost|nil
+M.get_last_host = function()
+  return vim.g.DEPLOY_LAST_HOST or nil
+end
+
+---@param host DeployHost
+M.set_last_host = function(host)
+  vim.g.DEPLOY_LAST_HOST = host
+end
+
 ---@param opts {msg: string, level?: integer, silent?: boolean}
 M.notify = function(opts)
   local silent = opts.silent or false
