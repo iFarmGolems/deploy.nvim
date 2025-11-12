868b0c2fe5c5215e46941c0dc9402cf52587d374
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2025-11-12 13:22:15 +0100

    Add rsync exit code map

--- lua/deploy/utils.lua
+++ lua/deploy/utils.lua
@@ -2,6 +2,10 @@ local nio = require("nio")
 
 local M = {}
 
+M.debug = function(...)
+  print(vim.inspect(...))
+end
+
 ---@return DeployHost|nil
 M.get_last_host = function()
   return vim.g.DEPLOY_LAST_HOST or nil
@@ -42,8 +46,28 @@ M.run_shell_command = nio.create(
   1
 )
 
-M.debug = function(...)
-  print(vim.inspect(...))
+-- codes taken from `man rsync`
+M.rsync_err_code_to_human = function(code)
+  local error_code_map = {
+    [4] = "Requested action not supported",
+    [5] = "Error starting client-server protocol",
+    [6] = "Daemon unable to append to log-file",
+    [10] = "Error in socket I/O",
+    [11] = "Error in file I/O",
+    [12] = "Error in rsync protocol data stream",
+    [13] = "Errors with program diagnostics",
+    [14] = "Error in IPC code",
+    [20] = "Received SIGUSR1 or SIGINT",
+    [21] = "Some error returned by waitpid()",
+    [22] = "Error allocating core memory buffers",
+    [23] = "Partial transfer due to error",
+    [24] = "Partial transfer due to vanished source files",
+    [25] = "The --max-delete limit stopped deletions",
+    [30] = "Timeout in data send/receive",
+    [35] = "Timeout waiting for daemon connection",
+  }
+
+  return error_code_map[code] and error_code_map[code] or "Unknown error code: " .. tostring(code)
 end
 
 return M
