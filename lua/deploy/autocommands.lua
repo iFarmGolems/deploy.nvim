97d340c9fe536d45a671e790bad1c1a607be65d9
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2025-11-12 12:44:20 +0100

    Add augroup

--- lua/deploy/autocommands.lua
+++ lua/deploy/autocommands.lua
@@ -1,6 +1,9 @@
 local lib = require("deploy.lib")
 
+local augroup = vim.api.nvim_create_augroup("DeployAutocommands", { clear = true })
+
 vim.api.nvim_create_autocmd("BufWritePost", {
+  group = augroup,
   callback = function(opts)
     if vim.g.DEPLOY_ON_SAVE then
       local source = opts.match
