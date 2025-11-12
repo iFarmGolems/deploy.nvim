3bc18248449cd495d2a82aeb29924774e32de9cc
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2025-11-12 12:34:02 +0100

    Utility function for calling shell commands

--- lua/deploy/utils.lua
+++ lua/deploy/utils.lua
@@ -1,3 +1,5 @@
+local nio = require("nio")
+
 local M = {}
 
 ---@return DeployHost|nil
@@ -21,6 +23,24 @@ M.set_last_custom_address = function(address)
   assert(type(address) == "string", "Address must be a string")
   vim.g.DEPLOY_LAST_CUSTOM_ADDRESS = address
 end
+
+---@type fun(opts: nio.process.RunOpts): ShellCommandResult
+M.run_shell_command = nio.create(
+  ---@param opts nio.process.RunOpts
+  function(opts)
+    local process = nio.process.run(opts)
+
+    if process == nil then
+      return { -1, "Failed to start process '" .. opts.cmd .. "'" }
+    end
+
+    local code = process.result(true)
+    local out = code == 0 and process.stdout.read() or process.stderr.read() or ""
+
+    return { code = code, out = out, command = opts.cmd .. " " .. table.concat(opts.args or {}, " ") }
+  end,
+  1
+)
 
 M.debug = function(...)
   print(vim.inspect(...))
